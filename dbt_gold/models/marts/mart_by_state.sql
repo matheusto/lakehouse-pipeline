35c418bc64895c9d855ec2de67996c885c841aaa
matheusto <matheus.t.o@outlook.com>
2026-04-18 16:26:20 +0000

    fix: add country filter to mart_by_state - exclude international records

--- dbt_gold/models/marts/mart_by_state.sql
+++ dbt_gold/models/marts/mart_by_state.sql
@@ -1,14 +1,17 @@
 SELECT
     state,
-    COUNT(*) AS total_breweries,
-    COUNT(DISTINCT city) as total_cities,
+    COUNT(*)                                        AS total_breweries,
+    COUNT(DISTINCT city)                            AS total_cities,
     COUNT(CASE WHEN website_url IS NOT NULL
-        THEN 1 END) AS breweries_with_website,
+               THEN 1 END)                          AS breweries_with_website,
     ROUND(
         COUNT(CASE WHEN website_url IS NOT NULL
-            THEN 1 END) * 100.0
-        / COUNT(*), 1    
-    ) as pct_with_website
+                   THEN 1 END) * 100.0
+        / COUNT(*), 1
+    )                                               AS pct_with_website
+
 FROM {{ ref('stg_breweries') }}
+WHERE country = 'United States'
+
 GROUP BY state
 ORDER BY total_breweries DESC
